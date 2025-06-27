40bc1fb842c5bb54e77da9266e9984fe4482aad3
Ezra Ramatong <mail.ezra@proton.me>
2025-06-27 22:01:33 +0200

    feat(tech-big-picture): add topics

--- .vitepress/sidebars/topics.ts
+++ .vitepress/sidebars/topics.ts
@@ -1,4 +1,8 @@
 import type { DefaultTheme } from "vitepress/types";
 import { onboardingTopics } from "./sections/onboarding";
+import { techBigPictureTopics } from "./sections/tech-big-picture";
 
-export const topicsSidebar: DefaultTheme.SidebarItem[] = [onboardingTopics];
+export const topicsSidebar: DefaultTheme.SidebarItem[] = [
+  onboardingTopics,
+  techBigPictureTopics,
+];
