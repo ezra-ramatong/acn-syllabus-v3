52b63fb6b174c0aaf3cc377fab63dd733f7ec656
Ezra Ramatong <mail.ezra@proton.me>
2025-06-28 00:11:19 +0200

    wip: add freecodecamps

--- .vitepress/sidebars/sections/web-foundations.ts
+++ .vitepress/sidebars/sections/web-foundations.ts
@@ -30,41 +30,69 @@ export const webFoundationsProjects: DefaultTheme.SidebarItem = {
           items: [
             {
               text: "Basic JavaScript",
-              link: "free-code-camp/1-basic-javascript/",
+              link: "free-code-camp/javascript/1-basic-javascript/",
             },
             {
               text: "ES6",
-              link: "free-code-camp/2-es6/",
+              link: "free-code-camp/javascript/2-es6/",
             },
             {
               text: "Regular Expressions",
-              link: "free-code-camp/3-regular-expressions/",
+              link: "free-code-camp/javascript/3-regular-expressions/",
             },
             {
               text: "Debugging",
-              link: "free-code-camp/4-debugging/",
+              link: "free-code-camp/javascript/4-debugging/",
             },
             {
               text: "Basic Data Structures",
-              link: "free-code-camp/5-basic-data-structures/",
+              link: "free-code-camp/javascript/5-basic-data-structures/",
             },
             {
               text: "Basic Algorithmic Scripting",
-              link: "free-code-camp/6-basic-algorithmic-scripting/",
+              link: "free-code-camp/javascript/6-basic-algorithmic-scripting/",
             },
             {
               text: "Object-Oriented Programming",
-              link: "free-code-camp/7-object-oriented-programming/",
+              link: "free-code-camp/javascript/7-object-oriented-programming/",
             },
             {
               text: "Functional Programming",
-              link: "free-code-camp/8-functional-programming/",
+              link: "free-code-camp/javascript/8-functional-programming/",
             },
             {
               text: "Intermediate Algorithmic Scripting",
-              link: "free-code-camp/9-intermediate-algorithmic-scripting/",
+              link: "free-code-camp/javascript/9-intermediate-algorithmic-scripting/",
             },
           ],
+        },
+        {
+          text: "Basic HTML and HTML5",
+          link: "free-code-camp/responsive-web-design/1-basic-html-and-html-5/",
+        },
+        {
+          text: "Basic CSS",
+          link: "free-code-camp/responsive-web-design/2-basic-css/",
+        },
+        {
+          text: "Applied Visual Design",
+          link: "free-code-camp/responsive-web-design/3-applied-visual-design/",
+        },
+        {
+          text: "Applied Accessibility",
+          link: "free-code-camp/responsive-web-design/4-applied-accessibility/",
+        },
+        {
+          text: "Responsive Web Design Principles",
+          link: "free-code-camp/responsive-web-design/5-responsive-web-design-principles/",
+        },
+        {
+          text: "CSS Flexbox",
+          link: "free-code-camp/responsive-web-design/6-css-flexbox/",
+        },
+        {
+          text: "CSS Grid",
+          link: "free-code-camp/responsive-web-design/7-css-grid/",
         },
       ],
     },
---
+++ content/projects/web-foundations/free-code-camp/responsive-web-design/2-basic-css/index.md
@@ -0,0 +1,27 @@
+---
+_db_id: 310
+content_type: project
+flavours:
+- none
+prerequisites:
+  hard:
+  - topics/free-code-camp/responsive-web-design/1-basic-html-and-html-5
+ready: true
+submission_type: link
+tags:
+- css
+- responsive web design
+- free-code-camp
+- skill/web_dev_basics
+title: FreeCodeCamp - Basic CSS
+---
+
+Make an account here if you haven't already: https://www.freecodecamp.org/learn
+
+Look under "Legacy Responsive Web Design Certification".
+
+You need to do all the "Basic CSS" exercises
+
+Please submit a link to your profile. E.g https://www.freecodecamp.org/fccfda32191-55c2-49ec-b6f4-26719c1dd7f4
+
+To make your profile useful, you need to go into your FreeCodeCamp settings and set all your privacy settings to "Public".
